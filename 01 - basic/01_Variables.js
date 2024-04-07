c537e021cd880e1bac6dcecc493f086398548c5d
vishalwaykar <vishalwaykar85@gmail.com>
2024-04-07 18:58:10 +0530

    Variable section

---
+++ 01 - basic/01_Variables.js
@@ -0,0 +1,9 @@
+const accountId = 122223;
+let accEmail = "vishalwaykar85@gmail.com";
+var accPassword = "Vishal231";
+accountCity = "Pune";
+
+// accountId = 2;
+
+console.log(accountId);
+console.table({accountId: accountId, accEmail:accEmail, accPassword: accPassword, accountCity: accountCity})
---
+++ 01 - basic/02_Data-types.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// alert(3 + 3);
+/*
+The above code is not allowed in node js engine it is only possible in browser engine.
+*/
