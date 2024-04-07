fc8f7f4dda6dec3812b12e0531192df8042619b8
vishalwaykar <vishalwaykar85@gmail.com>
2024-04-07 20:15:45 +0530

    added some theory notes

--- 01 - basic/datatype summary.js
+++ 01 - basic/datatype summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//datatypes are based on memory allocation
+// Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+// Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+// JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
+/*
+
+There are 2 types of datatype
+1. Primitive datatype:-
+    1.String
+    2. Number
+    3. Boolean (true / false)
+    4. null (iska matlab hai empty or zero nahi hai)
+    5. undefined (aapne variable declare kardiya hai but usmein aap kya value dalenge aapne decide nahi kiya hai)
+    6. Symbol (kisi bhi value ko unique banane ke liye use kiya jata hai)
+    7. BigInt (jo badi values hoti hai oo hum BigInt mein use karte hai)
+
+2. Preference datatype (Non Primitive datatype)
+    1. Array ( [ ] )
+    2. Object ( { } )
+    3. Function ( function( ){ } )
+*/
