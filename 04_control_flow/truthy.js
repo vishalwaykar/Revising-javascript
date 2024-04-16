2d9dc698152de36adad98c296897f7a4e153dc50
Vishal Waykar <36445890+vishalwaykar@users.noreply.github.com>
2024-04-16 23:44:13 +0530

    Update truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -40,4 +40,11 @@ console.log(val1);
 // condition ? true : false
 
 const iceTeaPrice = 100
-iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80")
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+// Nullish Coalescing Operator (??): null undefined
+/*
+Learn about above topic in detail understand how it works with upcoming project videos.
+
+
+*/ 
